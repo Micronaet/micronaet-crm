e01877dc139a9b154e9ef1e85f8fbab3f2299569
nicothebrush <nicola.riolini@gmail.com>
2022-01-31 15:18:00 +0100

    generic bugfix (fast message)

--- imap_download_email/imap_server.py
+++ imap_download_email/imap_server.py
@@ -88,7 +88,6 @@ class ImapServer(orm.Model):
             ))
 
         # Read all server:
-        import pdb; pdb.set_trace()
         for address in self.browse(cr, uid, ids, context=context):
             server = address.host  # '%s:%s' % (address.host, address.port)
             store_as_file = address.store_as_file
