584e4ff9b33db89da6332efd7c0908838a26edaf
nicothebrush <nicola.riolini@gmail.com>
2024-10-18 17:51:36 +0200

    generic bugfix (fast message)

--- base_geo_localize/models/res_partner.py
+++ base_geo_localize/models/res_partner.py
@@ -26,7 +26,9 @@ from openerp.tools.translate import _
 from geopy.geocoders import Nominatim
 from datetime import datetime
 
-google_url = 'https://www.google.com/maps/@{latitude},{longitude}'
+# google_url = 'https://www.google.com/maps/@{latitude},{longitude}'
+google_url = "https://www.google.com/maps/place/" \
+             "{latitude_grad}\"N+{longitude_grad}\"E/@{latitude},{longitude}"
 
 
 class ResPartner(osv.osv):
@@ -42,15 +44,32 @@ class ResPartner(osv.osv):
         'geo_altitude': fields.float('Geo Altitudine', digits=(16, 5)),
     }
 
+    def get_geo_grade(self, value):
+        """ Return 45.23456 in 45°23'44.6"
+        """
+        item1 = int(value)
+        value -= item1
+        item2 = int(value * 60.0)
+        value -= item2
+        item3 = value * 60.0
+        return "'{}°{}'{}\"".format(item1, item2, item3)
+
     # Button event:
     def open_geo_localize(self, cr, uid, ids, context=None):
         """
         """
         partner = self.browse(cr, uid, ids, context=context)[0]
+        latitude = partner.geo_latitude
+        latitude_grad = self.get_geo_grade(latitude)
+        longitude = partner.geo_longitude
+        longitude_grad = self.get_geo_grade(longitude)
         url = google_url.format(
-            latitude=partner.geo_latitude,
-            longitude=partner.geo_longitude,
+            latitude_grad=latitude_grad,
+            longitude_grad=longitude_grad,
+            latitude=latitude,
+            longitude=longitude,
             )
+
         return {
             'type': 'ir.actions.act_url',
             'url': url,
