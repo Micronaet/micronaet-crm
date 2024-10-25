cbd2e8643fae01c49fb26c0a6a56abf35e5884af
nicothebrush <nicola.riolini@gmail.com>
2024-10-25 11:38:04 +0200

    generic bugfix (fast message)

--- base_geo_localize/wizard/open_partner_map_wizard.py
+++ base_geo_localize/wizard/open_partner_map_wizard.py
@@ -313,7 +313,7 @@ class ResPartnerMapGeocodes(orm.TransientModel):
                                    "<br/>".format(
                                     email, email)
             except:
-                _logger.error('Error converting mail {}'.format(email))
+                _logger.error('Error converting mail')
 
             try:
                 website = (partner.website or '').strip()
