81b08aa645fd23b7f8aff03e085e81df067c3026
NicoTheBrush <nicola.riolini@gmail.com>
2026-02-03 16:17:07 +0100

    Fast commit

--- hubspot_connector/hubspot_connector.py
+++ hubspot_connector/hubspot_connector.py
@@ -150,6 +150,7 @@ class HubspotConnector(orm.Model):
                         'zip': partner.zip or '',
                         'partita_iva': partner.vat or '',
                         'agente_di_riferimento': partner.agent_id.name if partner.agent_id else '',
+                        'fascia_di_scontistica': partner.discount_rates or '',
                         # 'listino': partner.discount_rates or '',
                         # 'settore'
                         # 'macro_settore',
